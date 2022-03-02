bc055dcd23f9ae710e974abb05563eeae9d34ba8
Adhe Rama F <adheramaf7@gmail.com>
2022-03-02 16:22:24 +0700

    cors middleware

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors());
 app.use(decodeToken());
 
 app.use('/auth', authRouter);
