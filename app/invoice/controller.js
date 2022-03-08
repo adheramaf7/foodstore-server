f31eb9b4b852ebaea2d5355a93ad80627a6213af
Adhe Rama F <adheramaf7@gmail.com>
2022-03-08 10:41:23 +0700

    midtrans integration

--- app/invoice/controller.js
+++ app/invoice/controller.js
@@ -1,6 +1,8 @@
 const { subject } = require('@casl/ability');
 const Invoice = require('./model');
 const { policyFor } = require('../policy');
+const midtrans = require('midtrans-client');
+const config = require('../config');
 
 async function show(req, res, next) {
   try {
@@ -31,4 +33,97 @@ async function show(req, res, next) {
   }
 }
 
-module.exports = { show };
+let snap = new midtrans.Snap({
+  isProduction: config.midtrans.isProduction,
+  serverKey: config.midtrans.serverKey,
+  clientKey: config.midtrans.clientKey,
+});
+
+async function initiatePayment(req, res) {
+  try {
+    let { order_id } = req.params;
+
+    let invoice = await Invoice.findOne({ order: order_id }).populate('order').populate('user');
+
+    if (!invoice) {
+      return res.json({
+        error: 1,
+        message: 'Invoice Not Found',
+      });
+    }
+
+    let parameter = {
+      transaction_details: {
+        order_id: invoice.order._id,
+        gross_amount: invoice.total,
+      },
+      credit_card: {
+        secure: true,
+      },
+      customer_details: {
+        first_name: invoice.user.full_name,
+        email: invoice.user.email,
+      },
+    };
+
+    let response = await snap.createTransaction(parameter);
+
+    return res.json(response);
+  } catch (error) {
+    console.log(error);
+
+    return res.json({
+      error: 1,
+      message: 'Something when wrong',
+      errors: error?.message,
+    });
+  }
+}
+
+async function handleMidtransNotification(req, res) {
+  try {
+    let statusResponse = await snap.transaction.notification(req.body);
+
+    let orderId = statusResponse.order_id;
+    let transactionStatus = statusResponse.transaction_status;
+    let fraudStatus = statusResponse.fraud_status;
+
+    //untuk handle pembayaran kartu kredit
+    if (transactionStatus === 'capture') {
+      if (fraudStatus === 'challenge') {
+        await snap.transaction.approve(orderId);
+
+        await Invoice.findOneAndUpdate({ order: orderId }, { payment_status: 'paid' });
+
+        await Order.findOneAndUpdate({ _id: orderId }, { status: 'processing' });
+
+        return res.json('success');
+      }
+
+      if (fraudStatus === 'accept') {
+        await Invoice.findOneAndUpdate({ order: orderId }, { payment_status: 'paid' });
+
+        await Order.findOneAndUpdate({ _id: orderId }, { status: 'processing' });
+
+        return res.json('success');
+      }
+
+      return res.json('ok');
+    }
+
+    // (untuk non kartu kredit, artinya berhasil juga)
+    if (transactionStatus === 'settlement') {
+      await Invoice.findOneAndUpdate({ order: orderId }, { payment_status: 'paid' }, { new: true });
+
+      await Order.findOneAndUpdate({ _id: orderId }, { status: 'delivered' });
+
+      return res.json('success');
+    }
+
+    return res.json('ok');
+  } catch (error) {
+    return res.status(500).json('Something went wrong');
+  }
+}
+
+module.exports = { show, initiatePayment, handleMidtransNotification };
--- app/invoice/router.js
+++ app/invoice/router.js
@@ -2,5 +2,7 @@ const router = require('express').Router();
 const controller = require('./controller');
 
 router.get('/invoices/:order_id', controller.show);
+router.get('/invoices/:order_id/initiate-payment', controller.initiatePayment);
+router.get('/invoices/:order_id/midtrans-payment-notification', controller.handleMidtransNotification);
 
 module.exports = router;
